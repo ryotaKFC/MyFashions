0a0ccae6deac9b7b005295233f1f447a712f2ccc
ryotaKFC <ryotamokatoka@gmail.com>
2025-07-03 22:59:12 +0900

    カレンダー別ファイルにしました

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,32 +5,8 @@
 <p>ようこそ、{{ Auth::user()->name }}さん</p>
 
 <!-- カレンダー -->
-<div style="width: 50%;margin: auto" id='calendar'></div>
-
+@include('components.calendar')
 <!-- コーデ登録ボタン -->
 <p id="create-button"><a href="{{ route('fashions.create') }}">今日のコーデ登録</a></p>
 
-
-<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        const calendarEl = document.getElementById('calendar');
-
-        const calendar = new FullCalendar.Calendar(calendarEl, {
-            // initialView: 'listWeek',
-            initialView: 'dayGridMonth',
-            events: '/api/fashions',
-            eventContent: function(arg) {
-                const imageUrl = arg.event.extendedProps.image_url;
-                let customHtml = `<img src="${imageUrl}" style="width:100%; height:50px; object-fit:cover; border-radius:8px;" />`;
-                return { html: customHtml };
-            },
-        });
-        calendar.render();
-    });
-</script>
-
-
-
-@include('fashions.fashions')
 @endsection()
