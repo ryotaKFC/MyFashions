5752fde341a507b103afe849786868a81dd52a63
ryotaKFC <ryotamokatoka@gmail.com>
2025-07-06 15:45:15 +0900

    お気に入りボタンを写真の左上に変更

--- resources/views/fashions/fashions.blade.php
+++ resources/views/fashions/fashions.blade.php
@@ -4,6 +4,22 @@
 @else
 <fashion class="fashion-item">
     <div class="fashion-photo">
+        <!-- お気に入りボタン -->
+        <div class="fashion-control">
+            @if (!Auth::user()->is_bookmark(fashionId: $fashion->id))
+            <form action="{{ route('bookmark.store', $fashion) }}" method="post">
+                @csrf
+                <button class="favorite-btn">☆</button>
+            </form>
+            @else 
+            <form action="{{ route('bookmark.destroy', $fashion) }}" method="post">
+                @csrf
+                @method('delete')
+                <button class="favorite-btn">★</button>
+            </form>
+            @endif
+        </div>
+        <!-- ファッションの画像 -->
         <a href="{{ route('fashions.show', $fashion) }}">
             <img src="{{ asset('storage/avatar/' . $fashion->photo_path) }}" width="200px">
         </a>
@@ -11,36 +27,24 @@
     <div class="fashion-info">
         <div class="fashion-info-item">#{{ $fashion->season }}</div>
         <div class="fashion-info-item">#{{ $fashion->weather }}</div>
-        <div class="fashion-info-item">#{{ $fashion->temperature }}</div>
-        <div class="fashion-info-item">#{{ $fashion->humidity }}</div>
+        <div class="fashion-info-item">#気温{{ $fashion->temperature }}℃</div>
+        <div class="fashion-info-item">#湿度{{ $fashion->humidity }}%</div>
         <div class="fashion-info-item">#{{ $fashion->luck }}</div>
         <div class="fashion-info-item">#{{ $fashion->comment }}</div>
     </div>
     <div class="fashion-info-created_at">{{ $fashion->created_at }}</div>
-    <div class="article-control">
-        @if (!Auth::user()->is_bookmark(fashionId: $fashion->id))
-        <form action="{{ route('bookmark.store', $fashion) }}" method="post">
-            @csrf
-            <button>☆</button>
-        </form>
-        @else 
-        <form action="{{ route('bookmark.destroy', $fashion) }}" method="post">
-            @csrf
-            @method('delete')
-            <button>★</button>
-        </form>
-        @endif
-    </div>
+
+    
 </fashion>
 @endif
 @endforeach
 {{ $fashions->links() }}
 
 
-<!-- <style>
-    .fashion-photo {
+<style>
+    /* .fashion-photo {
         text-align: center;
-    }
+    } */
     .fashion-info {
         display: flex;
     }
@@ -48,4 +52,27 @@
         color:rgb(65, 142, 230);
         margin: 0px 3px;
     }
-</style> -->
+
+    /* お気に入りボタンのスタイル */
+    .favorite-btn {
+        background-color: rgba(0, 0, 0, 0);
+        color: rgb(234, 234, 41);
+        font-size: 25px;
+        border: 0px;
+        -webkit-text-stroke: 1px rgb(180, 180, 180);
+        paint-order: stroke;
+    }
+
+    /* お気に入りボタン左上にする処理 */
+    .fashion-photo {
+        position: relative;
+        display: inline-block;
+    }
+    .fashion-control {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        z-index: 2;
+    }
+
+</style>
