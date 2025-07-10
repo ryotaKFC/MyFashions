fea5cb8d092968a0e12b896627a6d075a6dafe02
ryotaKFC <ryotamokatoka@gmail.com>
2025-07-10 20:50:36 +0900

    いらないとこ変えた

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -66,62 +66,6 @@
       reader.readAsDataURL(file);
     }
   });
-
-
-  // // 季節自動入力
-  // const month = new Date().getMonth() + 1; // 0月始まりなので +1
-  // let season = '';
-  // if ([3, 4, 5].includes(month)) {
-  //     season = '春';
-  // } else if ([6, 7, 8].includes(month)) {
-  //     season = '夏';
-  // } else if ([9, 10, 11].includes(month)) {
-  //     season = '秋';
-  // } else {
-  //     season = '冬';
-  // }
-// document.querySelector('select[name="season"]').value = season;
-//   // 気温関係自動で入力
-//       navigator.geolocation.getCurrentPosition(success => {
-//         const lat = success.coords.latitude;
-//         const lon = success.coords.longitude;
-
-//         fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,weather_code&timezone=Asia%2FTokyo`)
-//             .then(response => response.json())
-//             .then(data => {
-//                 const current = data.current;
-//                 const temperature = Math.round(current.temperature_2m);
-//                 const humidity = current.relative_humidity_2m;
-//                 const weatherCode = current.weather_code;
-
-//                 // 天気コード → 天気表現（例: 晴れ、曇り、雨）
-//                 const weatherMap = {
-//                     0: '晴れ',
-//                     1: '晴れ',
-//                     2: '曇り',
-//                     3: '曇り',
-//                     45: '曇り',
-//                     48: '曇り',
-//                     51: '雨',
-//                     61: '雨',
-//                     63: '雨',
-//                     65: '雨',
-//                     71: '雪',
-//                     73: '雪',
-//                     75: '雪',
-//                     95: '雨',
-//                     99: '雨'
-//                 };
-//                 const weather = weatherMap[weatherCode] || '不明';
-
-//                 // 値を input 要素にセット
-//                 document.querySelector('input[name="temperature"]').value = temperature;
-//                 document.querySelector('input[name="humidity"]').value = humidity;
-//                 document.querySelector('select[name="weather"]').value = weather;
-//             });
-//     }, error => {
-//         console.error('位置情報の取得に失敗しました:', error);
-//     });
 </script>
 <style>
 /* フォーム全体 */
