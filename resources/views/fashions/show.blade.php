1f3103c112dd441d9a3d719bc8b80441ff0ae678
ryotaKFC <ryotamokatoka@gmail.com>
2025-07-11 14:27:21 +0900

    い

--- resources/views/fashions/show.blade.php
+++ resources/views/fashions/show.blade.php
@@ -42,9 +42,9 @@
             </form>
 
         @if (Auth::user()->email == "yasai@yasai.com")
-            <!-- <form onsubmit="return confirm('このアカウントでは無効にしてます')"> -->
-                <form action="{{ route('fashions.destroy', $fashion) }}" method="post">
+            <form onsubmit="return confirm('このアカウントでは無効にしてます')">
         @else
+            <form action="{{ route('fashions.destroy', $fashion) }}" method="post">
         @endif
             @csrf
             @method('delete')
